710265e862276528114a73c3ad8dfabf8da3cd01
SpiceHook Solutions <spicehooksolutions@gmail.com>
2022-11-11 23:07:42 +0530

    front end template integration

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,6 +1,16 @@
-   	</div>
-   	<script>
-        CKEDITOR.replace( 'editor1' );
-    </script>
-  </body>
+<footer class="footer">
+    <div class="footer-wrap">
+        <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <?php echo date('Y');?> </span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Blashup Audiencemanager</span>
+        </div>
+    </div>
+</footer>
+</div>
+<!-- container-scroller -->
+<script>
+CKEDITOR.replace('editor1');
+</script>
+</body>
+
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,12 +1,155 @@
-<html>
-  <head>
-    <title>CI Blog</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
-    <script src="http://cdn.ckeditor.com/4.7.1/full/ckeditor.js"></script>
-  </head>
-  <body>
-  <nav class="navbar navbar-inverse">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Blashup Audiencemanager</title>
+    <!-- base:css -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/vendors/mdi/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/vendors/base/vendor.bundle.base.css">
+    <!-- endinject -->
+
+    <!-- inject:css -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/frontend/css/style.css">
+
+    <!-- base:js -->
+    <script src="<?php echo base_url(); ?>assets/frontend/vendors/base/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="<?php echo base_url(); ?>assets/frontend/js/template.js"></script>
+    <!-- endinject -->
+    <!-- plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <script src="<?php echo base_url(); ?>assets/frontend/vendors/chart.js/Chart.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/frontend/vendors/progressbar.js/progressbar.min.js"></script>
+    <script
+        src="<?php echo base_url(); ?>assets/frontend/vendors/chartjs-plugin-datalabels/chartjs-plugin-datalabels.js">
+    </script>
+    <script src="<?php echo base_url(); ?>assets/frontend/vendors/justgage/raphael-2.1.4.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/frontend/vendors/justgage/justgage.js"></script>
+    <script src="<?php echo base_url(); ?>assets/frontend/js/jquery.cookie.js" type="text/javascript"></script>
+    <!-- Custom js for this page-->
+    <script src="<?php echo base_url(); ?>assets/frontend/js/dashboard.js"></script>
+    <!-- End custom js for this page-->
+
+    <!-- endinject -->
+</head>
+
+<body>
+    <div class="container-scroller">
+        <div class="horizontal-menu">
+
+            <nav class="bottom-navbar">
+                <div class="container">
+                    <ul class="nav page-navigation">
+                        <li class="nav-item">
+
+                            <a class="navbar-brand brand-logo" href="<?php echo base_url(); ?>"><img
+                                    src="<?php echo base_url(); ?>assets/frontend/images/logo.svg" alt="logo" /></a>
+                            <a class="navbar-brand brand-logo-mini" href="<?php echo base_url(); ?>"><img
+                                    src="<?php echo base_url(); ?>assets/frontend/images/logo-mini.svg"
+                                    alt="logo" /></a>
+
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?php echo base_url(); ?>">
+                                <i class="mdi mdi-file-document-box menu-icon"></i>
+                                <span class="menu-title">Dashboard</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="mdi mdi-cube-outline menu-icon"></i>
+                                <span class="menu-title">UI Elements</span>
+                                <i class="menu-arrow"></i>
+                            </a>
+                            <div class="submenu">
+                                <ul>
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="../../pages/ui-features/buttons.html">Buttons</a></li>
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="../../pages/ui-features/typography.html">Typography</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../../pages/forms/basic_elements.html" class="nav-link">
+                                <i class="mdi mdi-chart-areaspline menu-icon"></i>
+                                <span class="menu-title">Form Elements</span>
+                                <i class="menu-arrow"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../../pages/charts/chartjs.html" class="nav-link">
+                                <i class="mdi mdi-finance menu-icon"></i>
+                                <span class="menu-title">Charts</span>
+                                <i class="menu-arrow"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../../pages/tables/basic-table.html" class="nav-link">
+                                <i class="mdi mdi-grid menu-icon"></i>
+                                <span class="menu-title">Tables</span>
+                                <i class="menu-arrow"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="../../pages/icons/mdi.html" class="nav-link">
+                                <i class="mdi mdi-emoticon menu-icon"></i>
+                                <span class="menu-title">Icons</span>
+                                <i class="menu-arrow"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="mdi mdi-codepen menu-icon"></i>
+                                <span class="menu-title">Sample Pages</span>
+                                <i class="menu-arrow"></i>
+                            </a>
+                            <div class="submenu">
+                                <ul class="submenu-item">
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="../../pages/samples/login.html">Login</a></li>
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="../../pages/samples/login-2.html">Login 2</a></li>
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="../../pages/samples/register.html">Register</a></li>
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="../../pages/samples/register-2.html">Register 2</a></li>
+                                    <li class="nav-item"><a class="nav-link"
+                                            href="../../pages/samples/lock-screen.html">Lockscreen</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="nav-item nav-profile dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
+                                <span class="nav-profile-name">Johnson</span>
+                                <span class="online-status"></span>
+                                <img src="<?php echo base_url(); ?>assets/frontend/images/faces/face28.png" alt="profile" />
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right navbar-dropdown"
+                                aria-labelledby="profileDropdown">
+                                <a class="dropdown-item">
+                                    <i class="mdi mdi-settings text-primary"></i>
+                                    Settings
+                                </a>
+                                <a class="dropdown-item">
+                                    <i class="mdi mdi-logout text-primary"></i>
+                                    Logout
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+
+        <!-- <nav class="navbar navbar-inverse">
   	<div class="container">
   		<div class="navbar-header">
   		<a class="navbar-brand" href="<?php echo base_url(); ?>">CI Blogs</a>	
@@ -30,43 +173,43 @@
        </ul>  
   		</div>
   	</div>
-  </nav>
+  </nav> -->
 
-  <div class="container">
 
-  <!-- Flash Messages -->
-    <?php if($this->session->flashdata('user_registered')): ?>
-      <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_registered').'</p>'; ?>
-    <?php endif; ?>
 
-     <?php if($this->session->flashdata('post_created')): ?>
-      <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_created').'</p>'; ?>
-    <?php endif; ?>
+        <!-- Flash Messages -->
+        <?php if($this->session->flashdata('user_registered')): ?>
+        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_registered').'</p>'; ?>
+        <?php endif; ?>
 
-     <?php if($this->session->flashdata('post_updated')): ?>
-      <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_updated').'</p>'; ?>
-    <?php endif; ?>
+        <?php if($this->session->flashdata('post_created')): ?>
+        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_created').'</p>'; ?>
+        <?php endif; ?>
 
-    <?php if($this->session->flashdata('category_created')): ?>
-      <?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_created').'</p>'; ?>
-    <?php endif; ?>
+        <?php if($this->session->flashdata('post_updated')): ?>
+        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_updated').'</p>'; ?>
+        <?php endif; ?>
 
-    <?php if($this->session->flashdata('post_deleted')): ?>
-      <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_deleted').'</p>'; ?>
-    <?php endif; ?>
+        <?php if($this->session->flashdata('category_created')): ?>
+        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_created').'</p>'; ?>
+        <?php endif; ?>
 
-    <?php if($this->session->flashdata('login_failed')): ?>
-      <?php echo '<p class="alert alert-danger">'.$this->session->flashdata('login_failed').'</p>'; ?>
-    <?php endif; ?>
+        <?php if($this->session->flashdata('post_deleted')): ?>
+        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('post_deleted').'</p>'; ?>
+        <?php endif; ?>
 
-    <?php if($this->session->flashdata('user_loggedin')): ?>
-      <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_loggedin').'</p>'; ?>
-    <?php endif; ?>
+        <?php if($this->session->flashdata('login_failed')): ?>
+        <?php echo '<p class="alert alert-danger">'.$this->session->flashdata('login_failed').'</p>'; ?>
+        <?php endif; ?>
 
-    <?php if($this->session->flashdata('user_loggedout')): ?>
-      <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_loggedout').'</p>'; ?>
-    <?php endif; ?>
+        <?php if($this->session->flashdata('user_loggedin')): ?>
+        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_loggedin').'</p>'; ?>
+        <?php endif; ?>
 
-     <?php if($this->session->flashdata('category_deleted')): ?>
-      <?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_deleted').'</p>'; ?>
-    <?php endif; ?>
+        <?php if($this->session->flashdata('user_loggedout')): ?>
+        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('user_loggedout').'</p>'; ?>
+        <?php endif; ?>
+
+        <?php if($this->session->flashdata('category_deleted')): ?>
+        <?php echo '<p class="alert alert-success">'.$this->session->flashdata('category_deleted').'</p>'; ?>
+        <?php endif; ?>
